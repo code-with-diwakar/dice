7868a6630e217df77aa6a3149477c40b5c73d1a7
divyam <mayvid0011@gmail.com>
2024-06-21 11:28:42 +0530

    UPDATED CODE

--- src/dice.js
+++ src/dice.js
@@ -1,26 +1,36 @@
 const image = document.getElementById("image");
 const roll = document.getElementById("roll");
 const number = document.getElementById("number");
-const image1=document.getElementById("image1");
-const roll1=document.getElementById("roll1");
-const number1=document.getElementById("number1")
-const disp=document.getElementById("disp")
+const image1 = document.getElementById("image1");
+const number1 = document.getElementById("number1");
+const disp = document.getElementById("disp");
+const reset = document.getElementById("reset");
 
-let randomNumber = Math.floor(Math.random()*6)+1;
-let randomNumber2=Math.floor(Math.random()*6)+1;
-roll.addEventListener("click",()=>{
-    number.innerHTML= randomNumber;
-    image.setAttribute("src",`./image/${randomNumber}.jpeg`);
-    number1.innerHTML=randomNumber2;
-    image1.setAttribute("src",`./image/${randomNumber2}.jpeg`)
+roll.addEventListener("click", () => {
+    let randomNumber = Math.floor(Math.random() * 6) + 1;
+    let randomNumber2 = Math.floor(Math.random() * 6) + 1;
+    
+    number.innerHTML = randomNumber;
+    image.setAttribute("src", `./image/${randomNumber}.jpeg`);
+    
+    number1.innerHTML = randomNumber2;
+    image1.setAttribute("src", `./image/${randomNumber2}.jpeg`);
+    
+    if (randomNumber2 > randomNumber) {
+        disp.innerHTML = "Player 2 wins!";
+    } else if (randomNumber2 < randomNumber) {
+        disp.innerHTML = "Player 1 wins!";
+    } else {
+        disp.innerHTML = "It's a draw!";
+    }
+});
 
-})
-if(randomNumber2 > randomNumber){
- roll1.innerHTML="Player 2 win"
-}
-
-else if(randomNumber2 < randomNumber){
-  roll1.innerHTML="player 1 win"
-}else{
-  roll1.innerHTML="match draw"
-}
+reset.addEventListener("click", () => {
+    number.innerHTML = "DISPLAYED NUMBER";
+    image.setAttribute("src", "");
+    
+    number1.innerHTML = "DISPLAYED NUMBER";
+    image1.setAttribute("src", "");
+    
+    disp.innerHTML = "";
+});
